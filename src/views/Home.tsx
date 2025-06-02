565b17e7d429b47e4ec20a66420cb570ae363762
karla.aquino <karlaaquino371@gmail.com>
2025-06-02 10:48:48 -0600

    Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <div>bienvenidos amiguitos</div>
-  )
+    <div>
+      <h1>Bienvenidos, amiguitos</h1>
+      <p>Esta es la página principal de nuestra aplicación. ¡Esperamos que la disfruten!</p>
+    </div>
+  );
 }
